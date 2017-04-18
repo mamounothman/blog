d71b43c79792ec8bcbed39ddfc5e4e73a77828a5
mamoun othman <mamounothman@gmail.com>
2017-04-18 17:48:59 +0300

    implemented the new CSS changes and layout for wordpress theme.

---
+++ header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+  
+  <head>
+    <title>Cello &bull; {{ title }} </title>
+  
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta name="description" content="libCello Official Website" />
+    <meta name="keywords" content="libCello, Cello, C, language" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">  
+    
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" />
+    <!-- <link rel="stylesheet" href="/static/css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="/static/css/main.css" type="text/css" />
+    <link rel="stylesheet" href="/static/css/code.css" type="text/css" /> -->
+    <link rel="icon" type="image/png" href="/static/img/favicon.png?v=2" />
+  
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/static/js/jquery.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/static/js/bootstrap.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/static/js/rainbow.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/static/js/generic.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/static/js/shell.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/static/js/sql.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/static/js/php.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/static/js/javascript.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/static/js/cello.js"></script>
+    <?php wp_head(); ?>
+
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-6 col-md-6">
+            <img src="<?php echo get_template_directory_uri(); ?>/static/img/logo.jpg" alt="logo" 
+              style="float:left; margin-right:20px; margin-top:20px;" />
+            <h2 style="margin-top:40px; margin-bottom:0px;"> Art 
+              <small style="font-size:50%;">Of Eclipse</small></h2>
+          </div>
+          <div class="col-xs-6 col-md-6">
+            <p class="text-right" style="margin-top:40px;">
+              &laquo; <a href="/">Home</a> &raquo;
+              <span class="marginlr"></span>
+              &laquo; <a href="/about">About</a> &raquo;
+              <span class="marginlr"></span>
+              
+              &laquo; <a
+                href="https://github.com/orangeduck/Cello" 
+                target="blank">Github</a> &raquo;
+            </p>
+          </div>
+        </div>
+        <hr />
---
+++ page.php
@@ -0,0 +1,7 @@
+<div class="row">
+  <div class="col-xs-12 col-md-12">
+    <div>
+      
+    </div>
+  </div>
+</div>
